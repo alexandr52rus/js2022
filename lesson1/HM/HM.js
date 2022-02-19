e1ce7c39e6c7e948b5820b3fbab8f978c26e3b36
Alexandr Anikeev <alexandranikeev@mail.ru>
2022-02-19 11:20:30 +0300

    Добавил первый скрипт

---
+++ lesson1/HM/HM.js
@@ -0,0 +1,12 @@
+const myPlanet = "Земля";
+console.log(myPlanet);
+let paperCount = "12 статей";
+console.log(paperCount);
+let hasCash = "Наличные";
+console.log(hasCash);
+const firstName="Alexandr";
+const lastName="Anikeev";
+const middleName="Igorevich"
+console.log(lastName,firstName,middleName);
+let currentTime="01:15";
+console.log(currentTime);
