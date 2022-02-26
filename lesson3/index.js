8b9d69466390ed0629a8b5d08b07cf53f4712806
Alexandr Anikeev <alexandranikeev@mail.ru>
2022-02-26 12:16:04 +0300

    Урок 3

---
+++ lesson3/index.js
@@ -0,0 +1,96 @@
+//number
+
+// console.log(1/0);
+// console.log(typeof NaN);
+
+
+//string
+
+// const myString = "строка";
+// console.log(myString);
+
+
+//Boolean
+
+// const isAdmin = true;
+// const isUser = false;
+
+
+//Null
+// const emptyVariable = null;
+
+
+//undefined
+
+// const userName;
+// console.log(userName);
+
+
+//Непремитивные типы данных
+//Object - объект
+
+// const someUser = "Petya"
+// const user = {
+//     name: someUser,
+//     lastName: "Ivanov",
+//     age: 23,
+//     citizenship: "Russia",
+// }
+// console.log(user.age);
+
+
+//оператор typeof
+
+// console.log (typeof null);
+
+
+//Строковое преобразование
+
+// const myVar = true;
+// const myVarToString = String(myVar);
+// console.log(typeof myVarToString);
+
+//Численное преобразование
+
+// const myVar = "123";
+// const myVarToNumber = Number(myVar);
+// console.log(typeof myVarToNumber);
+
+//Логическое преобразование
+
+// const myVar = 1;
+// console.log(!!myVar);
+
+
+//Математические операторы
+//Бинарный плюс
+
+// const myNumber = 1;
+// const newNumber = 2;
+// const myString = "строка";
+// console.log(myNumber + myString);
+
+
+//Сравнение строк
+
+// const string1 = "строка";
+// const string2 = "солнце";
+// console.log(string1 == string2);
+
+/* UNICOD */
+
+// const userTypes = {
+//     admin: "admin",
+//     user: "user",
+//     teacer: "teacer",
+//     contentManager: "contentManager",
+// };
+
+// const serverDataUserTypes = "user";
+// if (serverDataUserTypes !== userTypes.admin) {
+//     console.log("показать весь контент")
+// } else {
+//     console.log("показать ограниченный контент")
+// }
+
+
